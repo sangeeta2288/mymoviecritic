398342778b65494a773588020172bd9af3c6cded
Anamika Singh <asingh015@citymail.cuny.edu>
2017-04-26 16:29:46 -0400

    Created critic model , Updated Settings.py

--- Django_Api/myapp/models.py
+++ Django_Api/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Critic(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
