d49a7b53c277deb9226aa2e088e9f6cdc8eee6ca
Sangeeta Ashrit <sashrit000@citymail.cuny.edu>
2017-04-30 17:23:42 -0400

    Advanced Search : Genre based search is implemented

--- src/app/advancedsearch/advancedsearch.component.ts
+++ src/app/advancedsearch/advancedsearch.component.ts
@@ -16,6 +16,7 @@ export class AdvancedsearchComponent implements OnInit {
   api_key = 'e0e7c64ea07685380dd4068b5334d37b';
   commonLink = 'https://api.themoviedb.org/3/';
   imageLink = 'https://image.tmdb.org/t/p/w500/';
+  blankSearch = false;
 
   constructor(http: Http) {
     this.http = http;
@@ -29,5 +30,40 @@ export class AdvancedsearchComponent implements OnInit {
         .subscribe((res: Response) => {
           this.myGenres = res.json().genres;
          });
+  }
+
+  //called on submit (click)
+  Submit() {
+  //If blank search warn user.
+  if(!this.Genre || !this.Genre.length){
+    this.blankSearch = true;
+    return;
+  }
+  this.results = [];
+  this.blankSearch = false;
+  console.log(this.Genre);//id list = [28,16]
+    this.selectedGenres = [];//genrelist = [Action, adventure, comedy....]
+    //map 'Genre' ids to selectedGenres names
+    this.Genre.forEach((item) => {
+        const filteredGenres = this.myGenres.filter((g) => { return g.id == item; });
+        filteredGenres.forEach((genre) => {
+            if(!this.selectedGenres.includes(genre.name)){
+                this.selectedGenres.push(genre.name);
+            }
+        });
+    });
+    //form of url = 'https://api.themoviedb.org/3/genre/28,12/movies?api_key=e0e7c64ea07685380dd4068b5334d37b';
+    let extLink = `${this.commonLink}genre/${this.Genre}/movies?api_key=${this.api_key}`;
+
+    this.http.request(extLink)
+     .subscribe((res: Response)=> {
+        this.results = (res.json()).results;
+     });
+  }
+
+  //called on Reset (click)
+  Reset(){
+    this.Genre = [];
+    this.results = [];
   }
 }
