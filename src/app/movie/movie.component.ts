ae92e64bd8c27fc5e5280cda9ccdd672a0f98f5f
deekshithraop <dpolusa000@citymail.cuny.edu>
2017-04-30 16:35:32 -0400

    added @componet decorater and selectors

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -2,3 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import {imdb} from 'imdb-api';
 
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+
+   
+}
+
