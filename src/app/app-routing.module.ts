f2be0dac5e3eee4d6d9269e304121a2a6f0a4518
Sangeeta Ashrit <sashrit000@citymail.cuny.edu>
2017-05-01 14:04:47 -0400

    Advanced Search : Genre based search is implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { AdvancedsearchComponent } from './advancedsearch/advancedsearch.component';
 ;
 
 const appRoutes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
+  { path: 'advancedsearch', component: AdvancedsearchComponent }
 ];
 
 @NgModule({
