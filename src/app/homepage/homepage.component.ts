a32be1528e1bae3d748fdb62e90ec350049b07e9
DESKTOP-QUKM426\AVINASH <abenger000@citymail.cuny.edu>
2017-05-02 00:34:00 -0400

    Added new section in home page - Top rated movies

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -10,7 +10,7 @@ import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
 export class HomePageComponent implements OnInit {
 	http: Http;
 	nowplayingmovies = [];
-	
+	topratedmovies = [];
 	api_key = 'e0e7c64ea07685380dd4068b5334d37b';
 	commonLink = 'https://api.themoviedb.org/3/';
 	imageLink = 'https://image.tmdb.org/t/p/w500/';
@@ -22,11 +22,18 @@ export class HomePageComponent implements OnInit {
 	
 	ngOnInit() {
 		let nowplayingApiLink = `${this.commonLink}movie/now_playing?api_key=${this.api_key}`;
-	
+		let topratedApiLink = `${this.commonLink}movie/top_rated?api_key=${this.api_key}`;
+
 		console.log(nowplayingApiLink);
 		this.http.request(nowplayingApiLink)
 		 .subscribe((res: Response)=> {
 			this.nowplayingApiLink = (res.json()).results;
+		 });
+		
+		console.log(topratedApiLink);
+		this.http.request(topratedApiLink)
+		 .subscribe((res: Response)=> {
+			this.topratedmovies = (res.json()).results;
 		 });
 		
 	}  
