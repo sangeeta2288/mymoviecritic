fc7cb88c29404314f51c670f8d8ec1ca74b9cb8b
DESKTOP-QUKM426\AVINASH <abenger000@citymail.cuny.edu>
2017-05-02 00:22:42 -0400

    Added new component for homepage

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomePageComponent implements OnInit {
+	http: Http;
+	nowplayingmovies = [];
+	
+	api_key = 'e0e7c64ea07685380dd4068b5334d37b';
+	commonLink = 'https://api.themoviedb.org/3/';
+	imageLink = 'https://image.tmdb.org/t/p/w500/';
+
+	constructor(http: Http) {
+		this.http = http;
+	}
+
+	
+	ngOnInit() {
+		let nowplayingApiLink = `${this.commonLink}movie/now_playing?api_key=${this.api_key}`;
+	
+		console.log(nowplayingApiLink);
+		this.http.request(nowplayingApiLink)
+		 .subscribe((res: Response)=> {
+			this.nowplayingApiLink = (res.json()).results;
+		 });
+		
+	}  
+}
