3161499ed999fe744fda36cfd9bbd20e517fa886
deekshithraop <dpolusa000@citymail.cuny.edu>
2017-05-03 10:33:22 -0400

    added movie component and home page coponent to router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AdvancedsearchComponent } from './advancedsearch/advancedsearch.component';
 ;
+import { MovieComponent } from './movie/movie.component';
+import { HomePageComponent } from './homepage/homepage.component';
+
 
 const appRoutes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
-  { path: 'advancedsearch', component: AdvancedsearchComponent }
+  { path: 'advancedsearch', component: AdvancedsearchComponent },
+  { path: 'movie', component: MovieComponent },
 ];
 
 @NgModule({
