8d696aff7b0d1feeebcf180d9fbca7dfb7dfd400
Sangeeta Ashrit <sashrit000@citymail.cuny.edu>
2017-05-04 11:33:13 -0400

    app module contact page import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
 import {RatingModule} from "ngx-rating";
-
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './homepage/homepage.component';
-
 import { AdvancedsearchComponent } from './advancedsearch/advancedsearch.component';
 import { ContactComponent } from './contact/contact.component';
-
 import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
@@ -25,8 +22,8 @@ import { MovieComponent } from './movie/movie.component';
     HeaderComponent,
     AdvancedsearchComponent,
     ContactComponent,
-	HomePageComponent,
-  MovieComponent
+	  HomePageComponent,
+    MovieComponent
 
   ],
   imports: [
