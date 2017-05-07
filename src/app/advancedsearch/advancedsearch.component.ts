7897fcdf7be035b346ab3118648c653f6c78759c
Sangeeta Ashrit <sashrit000@citymail.cuny.edu>
2017-05-06 21:57:53 -0400

    Moved Genre selection to it's own function

--- src/app/advancedsearch/advancedsearch.component.ts
+++ src/app/advancedsearch/advancedsearch.component.ts
@@ -40,36 +40,43 @@ export class AdvancedsearchComponent implements OnInit {
 
   //called on submit (click)
   Submit() {
-  //If blank search warn user.
-  if((!this.Genre || !this.Genre.length) && (!this.Title || this.Title == "")){
-    this.blankSearch = true;
-    return;
-  }
-  this.results = [];
-  this.blankSearch = false;
-  console.log(this.Genre);//id list = [28,16]
-    this.selectedGenres = [];//genrelist = [Action, adventure, comedy....]
-    //map 'Genre' ids to selectedGenres names
-    this.Genre.forEach((item) => {
-        const filteredGenres = this.myGenres.filter((g) => { return g.id == item; });
-        filteredGenres.forEach((genre) => {
-            if(!this.selectedGenres.includes(genre.name)){
-                this.selectedGenres.push(genre.name);
-            }
-        });
-    });
+    //If blank search warn user.
+    if((!this.Genre || !this.Genre.length) && (!this.Title || this.Title == "")){
+      this.blankSearch = true;
+      return;
+    }
+    this.results = [];
+    this.blankSearch = false;
+
     //form of url = 'https://api.themoviedb.org/3/genre/28,12/movies?api_key=e0e7c64ea07685380dd4068b5334d37b';
     let extLink = `${this.commonLink}genre/${this.Genre}/movies?api_key=${this.api_key}`;
 
     this.http.request(extLink)
-     .subscribe((res: Response)=> {
+    .subscribe((res: Response)=> {
         this.results = (res.json()).results;
-     });
+    });
   }
 
   //called on Reset (click)
   Reset(){
     this.Genre = [];
     this.results = [];
+  }
+
+  GetSelectedGenres(): any[] {
+    if(!this.Genre || !this.Genre.length)
+      return([]);
+
+    let selectedGenres = [];//genrelist = [Action, adventure, comedy....]
+    //map 'Genre' ids to selectedGenres names
+    this.Genre.forEach((item) => {
+      const filteredGenres = this.myGenres.filter((g) => { return g.id == item; });
+      filteredGenres.forEach((genre) => {
+        if(!selectedGenres.includes(genre.name)){
+          selectedGenres.push(genre.name);
+        }
+      });
+    });
+    return(selectedGenres);
   }
 }
