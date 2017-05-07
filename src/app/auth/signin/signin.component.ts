f43a6a5519d40aab8b2958eb5d6a1d0fc5faf6bd
deekshithraop <dpolusa000@citymail.cuny.edu>
2017-05-07 11:24:42 -0400

    use authentication service to login with username and password

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Http, Headers } from '@angular/http';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
+
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -11,29 +14,19 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class SigninComponent implements OnInit {
 
   
+
   constructor(private _http: Http,
               private route: ActivatedRoute,
+              private authenticationService: AuthenticationService,
               private router: Router) {}
 
   ngOnInit() {
+     
   }
 
   onSignin(form: NgForm) {
     const username = form.value.username;
     const password = form.value.password;
-    let params = JSON.stringify({username: username , password: password });
-    console.log(params);
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    this._http.post('http://127.0.0.1:8000/api/v1/user/login/', params , {
-      headers: headers
-    }).subscribe
-   
-    (
-      data => {
-        this.router.navigate(['home']);
-      },
-    );
-  }
+        this.authenticationService.login(username,password)
 
 }
