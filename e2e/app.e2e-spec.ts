359504a7048b79b13f2a94b21ae0ad3962161406
Sangeeta Ashrit <sashrit000@citymail.cuny.edu>
2017-05-09 12:11:39 -0400

    e2e testing script for scenario1 of advanced search

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -13,3 +13,19 @@ describe('moviecritic App', () => {
     expect(warningText.getText()).toEqual('At-least one of Genre or Title value should be present to perform search!!');
   });
 
+  it('should test successful search scenario 1 on advanced search page - Furious 7', () => {
+    browser.get(`${commonUrl}/advancedsearch`);
+    browser.waitForAngular();
+
+    element(by.id("TitleField")).sendKeys('speed');
+    browser.waitForAngular();
+    element(by.css(".btn.btn-success")).click();
+    browser.waitForAngular();
+
+    var h3_elements = element.all(by.tagName("h3"));
+    expect(h3_elements.getText()).toContain('Furious 7');
+  });
+
+
+});
+
