1804a4cff5936aaa0909247bd78d2a9013d1d52e
Sangeeta Ashrit <sashrit000@citymail.cuny.edu>
2017-05-09 12:10:13 -0400

    first script of e2e testing for validating black fields advanced search

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -3,3 +3,13 @@ var fs = require('fs');
 describe('moviecritic App', () => {
   //let commonUrl = 'https://mymoviecritic.herokuapp.com/static';
   let commonUrl = 'http://localhost:4200';
+
+  it('should test blank search warning on advanced search page', () => {
+    browser.get(`${commonUrl}/advancedsearch`);
+    browser.waitForAngular();
+    element(by.css(".btn.btn-success")).click();
+    browser.waitForAngular();
+    var warningText = element(by.css('.alert.alert-danger'));
+    expect(warningText.getText()).toEqual('At-least one of Genre or Title value should be present to perform search!!');
+  });
+
