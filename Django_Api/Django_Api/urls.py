652506a0e525bdf68fc31d0c5fb81238d3ad9add
deekshithraop <dpolusa000@citymail.cuny.edu>
2017-05-16 13:42:06 -0400

    Added Index view to the urls

--- Django_Api/Django_Api/urls.py
+++ Django_Api/Django_Api/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from myapp.api import v1_api
+from myapp import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$',views.index,name='index'),
     url(r'^api/', include(v1_api.urls)),
     url(r'^admin/', include(admin.site.urls)),
+    
 )
